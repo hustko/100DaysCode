9fa0d15cf4bccd4c01301abf91dc57a1f0b0f885
hustko <dhahamthi@gmail.com>
2024-07-27 00:07:39 +1000

    updated

--- Day3/project3.py
+++ Day3/project3.py
@@ -26,12 +26,12 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-choice_1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"?').lower()
+choice_1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"?\n').lower()
 
 if choice_1 == 'left':
-    choice_2 = input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim.").lower()
+    choice_2 = input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim.\n").lower()
     if choice_2 == 'wait':
-        choice_3 = input("WYou arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?").lower()
+        choice_3 = input("WYou arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n").lower()
         if choice_3 == 'yellow':
             print("You found the treasure. You win!")
         elif choice_3 == 'blue': 
