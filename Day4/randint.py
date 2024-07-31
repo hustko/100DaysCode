48b45dd0f02287a4e1c035ef1735246f21cbe617
hustko <dhahamthi@gmail.com>
2024-07-31 12:05:36 +1000

    randomisation
    learning to use randint

---
+++ Day4/randint.py
@@ -0,0 +1,10 @@
+import random #import random module
+#random is like a car, one person cannot build the whole car, so each part is assigned to a person like tire module, engine module etc
+#when a system is built and the code is large and long it is split into module
+#below the module.py is imported
+import my_module
+
+print(my_module.pi)
+
+num = random.randint(1,10) #randint gives num between 1 and 10 inclusive
+print(num)
